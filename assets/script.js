e40a44ccd17c1a0506524cd862b360a3450b1b1e
unknown <vlada@forwardslashny.com>
2021-08-05 14:23:20 +0200

    clouds test

--- assets/script.js
+++ assets/script.js
@@ -32,4 +32,55 @@ $(document).ready(function (){
             $('.scroll-icon').removeClass('scroll-icon--show')
         }
     });
+
+    const screenWidth = $(window).width();
+    const siteWidth = $('.wrapper--inner').width();
+    const backgroundOffset = (screenWidth - siteWidth) / 2;
+
+    $('.header-shade').css({
+        right: '-' + backgroundOffset + 'px',
+        height: '888px'
+    })
+
+    $('#header-clouds-wrapp').css({
+        right: '-' + backgroundOffset + 'px',
+        height: '888px'
+    })
+
+
+
+    var scene = document.getElementById('header-clouds-wrapp');
+    var parallaxInstance = new Parallax(scene);
+
+    // const movementStrength = 150;
+    // const height = movementStrength / $(window).height();
+    // const width = movementStrength / $(window).width();
+    //
+    // $("#header-clouds-move").mousemove(function(e){
+    //     var pageX = e.pageX - ($(window).width() / 2);
+    //     var pageY = e.pageY - ($(window).height() / 2);
+    //     var newvalueX = width * pageX * -1 - 25;
+    //     var newvalueY = height * pageY * -1 - 50;
+    //
+    //     console.log($('#header-clouds').css("background-position-x"));
+    //     // $('#header-clouds').css("background-position-x", newvalueX+"px");
+    //     // $('#header-clouds').css("background-position-y", newvalueY+"px");
+    // });
+
+    // $(window).scroll(function(e){
+    //     // var pageX = e.pageX - ($(window).width() / 2);
+    //     var pageY = $(this).scrollTop() - ($(window).height() / 2);
+    //     // var newvalueX = width * pageX * -1 - 25;
+    //     var newvalueY = height * pageY * -1 - 50;
+    //     $('#header-clouds').css("background-position-y", newvalueY+"px");
+    // });
+
+
+    // const trigger = document.querySelector("#header-clouds-move");
+    // const el = document.querySelector("#header-clouds");
+    // trigger.addEventListener("mousemove", (e) => {
+    //     console.log(el.style.backgroundPositionX)
+    //     el.style.backgroundPositionX = el.style.backgroundPositionX + 3 + "px";
+    //     // el.style.backgroundPositionY = -e.offsetY - 500 + "px";
+    // });
 })
